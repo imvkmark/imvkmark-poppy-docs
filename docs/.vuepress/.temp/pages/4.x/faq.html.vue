bc11923602ff3168a12f2b3e5c85e99edc2f6aaf
duoli <zhaody901@126.com>
2023-02-23 23:04:50 +0800

    remove vuepress

--- docs/.vuepress/.temp/pages/4.x/faq.html.vue
+++
@@ -1,7 +0,0 @@
-<template><div><h1 id="faq" tabindex="-1"><a class="header-anchor" href="#faq" aria-hidden="true">#</a> FAQ</h1>
-<h2 id="常见开发问题" tabindex="-1"><a class="header-anchor" href="#常见开发问题" aria-hidden="true">#</a> 常见开发问题</h2>
-<h3 id="target-app-services-userrepositoryinterface-is-not-instantiable" tabindex="-1"><a class="header-anchor" href="#target-app-services-userrepositoryinterface-is-not-instantiable" aria-hidden="true">#</a> Target [App\Services\UserRepositoryInterface] is not instantiable</h3>
-<p>查看下 ServiceProvider 是否已经加载</p>
-</div></template>
-
-
--- docs/.vuepress/.temp/pages/develop/index.html.vue
+++
@@ -1,9 +0,0 @@
-<template><div><h1 id="开发约定" tabindex="-1"><a class="header-anchor" href="#开发约定" aria-hidden="true">#</a> 开发约定</h1>
-<ul>
-<li>开发规范</li>
-<li>代码规范</li>
-<li>变动日志</li>
-</ul>
-</div></template>
-
-
