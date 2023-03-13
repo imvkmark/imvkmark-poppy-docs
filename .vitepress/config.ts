464a40045df625b4de5607afa43cdbeccf9c8a7e
duoli <zhaody901@126.com>
2023-03-13 20:08:22 +0800

    remove css

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -3,7 +3,6 @@ import { fileURLToPath, URL } from 'node:url'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-import 'element-plus/dist/index.css'
 
 /**
  * Create Simple Link
